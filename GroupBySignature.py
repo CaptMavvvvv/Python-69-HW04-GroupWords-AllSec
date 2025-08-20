5e6f52d4dc6534bae9d078a578ac474bb3579966
Anirach Mingkhwan <anirach.m@fitm.kmutnb.ac.th>
2025-08-20 13:15:42 +0700

    Create GroupBySignature.py

---
+++ GroupBySignature.py
@@ -0,0 +1,13 @@
+def group_by_signature(words: list) -> list:
+    pass
+
+if __name__ == "__main__":
+    # Example 1
+    words = ["abc", "bca", "cab", "bac", "xyz", "yxz", "zxy", "dog"]
+    print(group_by_signature(words))
+    # Output: [["abc", "bca", "cab", "bac"], ["xyz", "yxz", "zxy"], ["dog"]]
+
+    # Example 2
+    words = ["apple", "pale", "leap", "plea", "papel", "hello"]
+    print(group_by_signature(words))
+    # Output: [["apple", "papel"], ["pale", "leap", "plea"], ["hello"]]
